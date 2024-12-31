ece9ded2584446938aadeef10077d166f2c4d0b7
Dileep <dileepvraj50@gmail.com>
2024-12-31 18:15:34 +0530

    corrected sed command to update listen address of redis, and redirecting logs to log files

--- RoboShopWithShellScript/03_Redis.sh
+++ RoboShopWithShellScript/03_Redis.sh
@@ -9,15 +9,15 @@ LogFile="/tmp/"$0-$TimeStamp.log
 validateToUser
 
 # Installing Redis repo file as rpm.
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y >> $LogFile
 validateAction $? "Installing Redis repo"
 
 # Enable Redis 6.2 from package streams.
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y >> $LogFile
 validateAction $? "Enabling redis 6.2"
 
 # Installing redis.
-validate_and_install_packages redis
+validate_and_install_packages redis >> $LogFile
 
 # Usually Redis opens the port only to localhost(127.0.0.1), meaning this service can be accessed by 
 # the application that is hosted on this server only. However, we need to access this service to be 
@@ -25,15 +25,15 @@ validate_and_install_packages redis
 
 
 # Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
-sed -i 's/127.0.0.0/0.0.0.0/' /etc/redis.conf
+sed -i 's/127.0.0.0/0.0.0.0/g' /etc/redis.conf
 validateAction $? "Updating listen address from 127.0.0.1 to 0.0.0.0"
 
 # Enabling redis
-systemctl enale redis 
+systemctl enale redis >> $LogFile
 validateAction "Enabing redis"
 
 # Starting redis
-systemctl start redis
+systemctl start redis >> $LogFile
 validateAction "Starting redis"
 
 
