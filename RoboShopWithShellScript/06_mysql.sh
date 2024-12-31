d5a7bc51750e1d8cb47d8f61109a3c8500964503
Dileep <dileepvraj50@gmail.com>
2024-12-31 20:50:56 +0530

    commiting mysql.sh for first tym

---
+++ RoboShopWithShellScript/06_mysql.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source 0_utility_file.sh
+
+TimeStamp=$(date +%F-%H-%M-%S)
+LogFile="/tmp/"$0-$TimeStamp.sh
+
+# validating user.
+validateToUser
+
+# Disabling default mysql module.
+dnf disable module mysql -y >> $LogFile
+validateAction $? "Disabling default mysql module"
+
+# setting up mysql-5.7 repo file.
+cp '/home/DevOps/RoboShopWithShellScript/RepoFiles/mysql.repo' '/etc/yum.repos.d/mysql.repo'
+validateAction $? "Setting up mysql-5.7 repo"
+
+# Installing mysql server.
+validate_and_install_packages mysql-community-server -y
+
+# Enabling mysql server.
+systemctl enable mysqld
+validateAction $? "Enabling mysql server"
+
+# Starting mysql server.
+systemctl start mysqld
+validateAction $? "Starting mysqld server"
+
+# Changing mysql default password.
+mysql_secure_installation --set-root-pass RoboShop@1
+validateAction $? "Changing default password"
+ 
